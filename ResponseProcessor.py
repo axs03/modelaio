d7cdf677b9bf3b9f4c59f8a0a3dddedf5ce3b7c5
Aman Sahu <144287512+as9219@users.noreply.github.com>
2025-05-18 00:46:38 -0400

    updated tests and main

--- ResponseProcessor.py
+++ ResponseProcessor.py
@@ -1,16 +1,64 @@
 from gensim.models.doc2vec import TaggedDocument
+import re
+import os
 
 class ResponseProcessor:
-    def __init__(self, responses = None):
-        if not responses:
-            raise ValueError("Responses array is Empty")
-        else:
-            self.responses = responses
+    def __init__(self, file_paths = list()):
+        self.file_paths = file_paths
+        self.responses = dict()
 
+        self.read_files()
         self.make_chunks()
-    
-    def make_chunks(self):
-        pass
 
+        
+    def clean_file_path(self, file_path):
+        name = os.path.basename(file_path)
+        stem, _ = os.path.splitext(name) # _ = extension
+        return stem
+
+
+    def read_files(self):
+        for file_path in self.file_paths:
+            with open(file_path, "r") as file:
+                response = file.read()
+                key = self.clean_file_path(file_path)
+                self.responses[key] = response
+    
+
+    def make_chunks(self):
+        response_chunks = dict() # contains the mapped chunks for each response file
+        for key, response in self.responses.items():
+            headers = re.findall(r"^[*]{2}[a-zA-Z0-9]*[**]{2}$", response, re.MULTILINE) # get all markdown headers
+            stripped_headers = [header.strip("*") for header in headers] # remove the markdown stars
+            if not stripped_headers:
+                print (f"No headers found in {key}")
+                continue # skip file
+
+            paragraphs = re.split(r"[*]{2}[a-zA-Z0-9]*[**]{2}", response, re.MULTILINE)[1:] # remove false first element
+            
+            mapped_paragraphs = zip(stripped_headers, paragraphs, strict=True)
+            response_chunks[key] = mapped_paragraphs
+            
+        return response_chunks
+
+
+    # pretty print the chunks
     def get_chunks(self):
-        pass
+        for key, val in self.make_chunks().items():
+            print(f"File: {key}")
+            for i in val:
+                print(f"Header: {i[0]}")
+                print(f"Paragraph: {i[1]}")
+                print("" + "-"*10)
+            print("" + "-"*40)
+
+
+    def vectorize_chunks(self):
+        # create tagged documents from chunks in response processor
+        documents = [
+            TaggedDocument(words=chunk[1].split(), tags=[f"{key}_{chunk[0]}"]) 
+            for key, chunks in self.make_chunks().items() 
+            for chunk in chunks
+        ]
+        return documents
+
--- SimilarityModel.py
+++ SimilarityModel.py
@@ -2,9 +2,14 @@ import gensim
 from gensim.models.doc2vec import TaggedDocument
 import gensim.downloader as api
 from sklearn.metrics.pairwise import cosine_similarity
-
+import numpy as np
+import random
 
 class SimilarityModel:
+    SEED = 42
+    random.seed(SEED)
+    np.random.seed(SEED)
+
     def __init__(self, training_documents=None):
         if training_documents:
             self.training_data = training_documents
@@ -13,7 +18,7 @@ class SimilarityModel:
             self.set_train_data_text8()
         
         self.init_model()
-        # self.train()
+        self.train()
 
 
     def set_train_data_text8(self):
@@ -26,15 +31,22 @@ class SimilarityModel:
                 yield gensim.models.doc2vec.TaggedDocument(words = list_of_words, tags = [i])
 
         self.training_data = list(tagged_document(data)) # list of TaggedDocument objects
+    
+
+    def set_train_data(self, training_documents):
+        # can use an array of tagged docs called documents = [] to train the model
+        # to process chunks, we can make each chunk a tagged document
+        pass
 
 
-    def init_model(self, vector_size=50, window=2, min_count=1, workers=4, epochs=20):
+    def init_model(self, vector_size: int=50, window: int=2, min_count: int=1, workers: int=1, epochs: int=20, seed=SEED):
         # Initialize the Doc2Vec model
         self.model = gensim.models.Doc2Vec(vector_size=vector_size,  # Dimensionality of the document vectors
                                            window=window,         # Maximum distance between the current and predicted word within a sentence
                                            min_count=min_count,      # Ignores all words with total frequency lower than this
                                            workers=workers,        # Number of CPU cores to use for training
-                                           epochs=epochs)        # Number of training epochs   
+                                           epochs=epochs,         # Number of training epochs 
+                                           seed=seed)          
 
         # build the vocabulary from the training data
         self.model.build_vocab(self.training_data)
@@ -48,12 +60,17 @@ class SimilarityModel:
 
     def get_similarity(self, response1: TaggedDocument, response2:TaggedDocument):
         # Infer vectors for the documents
-        vector1 = self.model.infer_vector(response1.words)
-        vector2 = self.model.infer_vector(response2.words)
+        vector1 = self.model.infer_vector(response1.words,
+                                          epochs=100,
+                                          alpha=0.025,)
+        vector2 = self.model.infer_vector(response2.words,
+                                          epochs=100,
+                                          alpha=0.025,)
 
         # Calculate cosine similarity
         similarity = cosine_similarity([vector1], [vector2])[0][0]
         return similarity
+    
     
     def get_training_data(self):
         return self.training_data
